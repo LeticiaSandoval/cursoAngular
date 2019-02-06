6127b57c6396d22b7a95ec35e0abef711da31cd8
LeticiaSandoval <letysan2932@gmail.com>
2019-02-06 12:29:48 -0600

    Ejercicio formulario 1

--- formulario/src/app/app.component.ts
+++ formulario/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  formulario: FormGroup;
   title = 'formulario';
+  constructor(private fromBuilder:FormBuilder){
+    this.formulario = this.fromBuilder.group({
+      nombre: ['nombre por defecto', Validators.required],
+      apellido: ['nombre por defecto', Validators.required],
+      email: ['por defecto', [Validators.required, Validators.pattern("[^ @]*@[^ @]*")]]
+    })
+  }
+  
+  onSubmit(parametro){
+    console.log(parametro);
+  }
 }
