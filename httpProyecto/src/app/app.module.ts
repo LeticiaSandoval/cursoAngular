1e36cc15c0402e6e16ededdaa78a0184beec7eeb
LeticiaSandoval <letysan2932@gmail.com>
2019-02-06 17:31:45 -0600

    interfaz

--- httpProyecto/src/app/app.module.ts
+++ httpProyecto/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { PersonaService } from './servicio/persona.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
