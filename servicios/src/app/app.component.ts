d68d1c22ee19e8d5867cc9b6aa14ef9e650e1788
LeticiaSandoval <letysan2932@gmail.com>
2019-02-06 16:03:31 -0600

    Mostrar contenido del servicio

--- servicios/src/app/app.component.ts
+++ servicios/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductosService } from './servicio/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'servicios';
+  productos: any;
+  constructor(private ProductosService:ProductosService){
+    //console.log(JSON.stringify(ProductosService.productos));
+    this.productos = ProductosService.productos.rows;
+  }
 }
