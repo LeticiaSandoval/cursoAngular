26b648c8d44d75f9989d334f52e577657555524c
LeticiaSandoval <letysan2932@gmail.com>
2019-02-07 14:43:39 -0600

    notificaciones

--- notificaciones/src/app/app.component.ts
+++ notificaciones/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
+import { ToastsManager} from 'ng2-toastr/ng2-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notificaciones';
+  constructor(private toastsManager:  ToastsManager, ver: ViewContainerRef){
+    this.toastsManager.setRootViewContainerRef(ver);
+
+  }
+
+  opeacion1(){
+    this.toastsManager.success('Mensaje', 'titulo');
+
+  }
+  operacion2(){
+    this.toastsManager.error('mensaje', 'titulo');
+
+  }
+  operacion3(){
+    this.toastsManager.warning('mensaje', 'titulo');
+
+  }
+  operacion4(){
+    this.toastsManager.info('mensaje', 'titulo');
+
+  }
+  operacion5(){
+    this.toastsManager.custom('mensaje', 'titulo');
+
+  }
 }
--- notificaciones/src/app/app.module.ts
+++ notificaciones/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {  ToastModule} from 'ng2-toastr/ng2-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ToastsManager} from 'ng2-toastr/ng2-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastsManager
+
   ],
   providers: [],
   bootstrap: [AppComponent]
